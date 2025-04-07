b4e29de40be4d7634476c73e53c43d444a6b997a
Musa Adamu <msmajemusa4@gmail.com>
2025-04-07 09:50:29 +0100

    server update

--- server.js
+++ server.js
@@ -201,9 +201,7 @@ app.use(cors({
 
 // Set appropriate headers for all responses
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
     
     // Handle preflight requests
     if (req.method === 'OPTIONS') {
